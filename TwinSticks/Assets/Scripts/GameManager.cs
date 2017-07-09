a60158e2a45c1b75f809aaad96247d7b5beaf5ac
DESKTOP-681L4K3\robfe <orobfer@gmail.com>
2017-07-09 19:07:30 +0200

    End_Status

--- TwinSticks/Assets/Scripts/GameManager.cs
+++ TwinSticks/Assets/Scripts/GameManager.cs
@@ -6,11 +6,15 @@ using UnityStandardAssets.CrossPlatformInput;
 public class GameManager : MonoBehaviour {
 
     public bool recording = true;
+    private float fixedDeltaTime;
+    private bool isPaused = false;
 	// Use this for initialization
 	void Start () {
         PlayerPrefsManager.UnlockLevel(2);
         Debug.Log(PlayerPrefsManager.IsLevelUnlocked(1));
         Debug.Log(PlayerPrefsManager.IsLevelUnlocked(2));
+        fixedDeltaTime = Time.fixedDeltaTime;
+
     }
 	
 	// Update is called once per frame
@@ -23,5 +27,32 @@ public class GameManager : MonoBehaviour {
         {
             recording = true;
         }
-	}
+        if (Input.GetKeyDown(KeyCode.P) && isPaused)
+        {
+            ResumeGame();
+            isPaused = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.P) && !isPaused)
+        {
+            PauseGame();
+            isPaused = true;
+        }
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0f;
+        Time.fixedDeltaTime = 0f;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = fixedDeltaTime;
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        isPaused = pause;
+    }
 }
